3afa80eeb4ffa7d0f6ee962d98b84a3fc8100eed
Pavel <rezusx@mail.ru>
2024-02-10 14:30:56 +0300

    Initial Commit

---
+++ HW2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2-1
+//
+//  Created by Pavel Dolgopolov on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
